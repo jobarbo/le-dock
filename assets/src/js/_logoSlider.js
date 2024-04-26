291e004ca556922359664978adeee4f787324256
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-04-26 17:20:13 -0400

    correction to about

--- assets/src/js/_logoSlider.js
+++ assets/src/js/_logoSlider.js
@@ -1,5 +1,5 @@
 import Splide from "@splidejs/splide";
-
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 class LogoSlider {
   constructor() {
     this.slide = document.querySelector("[data-logo-slide]");
@@ -21,11 +21,18 @@ class LogoSlider {
     this.splide = new Splide(".projects__logo-container", {
       arrows: false,
       pagination: false,
+      drag: "free",
       perPage: 4,
       start: 0,
+      gap: "6rem",
+      padding: "5rem",
+      type: "loop",
+      autoScroll: {
+        speed: 2,
+      },
     });
 
-    this.splide.mount();
+    this.splide.mount({ AutoScroll });
   }
 }
 
