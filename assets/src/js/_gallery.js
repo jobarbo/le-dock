3ca3bcc07fa310a4ea548e8daf71606e617847fa
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-05-07 15:35:07 -0400

    mobile project

--- assets/src/js/_gallery.js
+++ assets/src/js/_gallery.js
@@ -5,14 +5,26 @@ class Gallery {
     this.gallery = document.querySelector("[data-masonry]");
 
     if (this.gallery) {
-      this.masonry = new MiniMasonry({
-        container: document.querySelector("[data-masonry]"),
-        item: document.querySelector("[data-masonry-item]"),
-        gutter: 10,
-        baseWidth: 500,
-        minify: true,
-        wedge: false,
-      });
+      // if device is tablet or bigger
+      if (window.innerWidth > 768) {
+        this.masonry = new MiniMasonry({
+          container: document.querySelector("[data-masonry]"),
+          item: document.querySelector("[data-masonry-item]"),
+          gutter: 10,
+          baseWidth: 500,
+          minify: true,
+          wedge: false,
+        });
+      } else {
+        this.masonry = new MiniMasonry({
+          container: document.querySelector("[data-masonry]"),
+          item: document.querySelector("[data-masonry-item]"),
+          gutter: 10,
+          baseWidth: 300,
+          minify: true,
+          wedge: false,
+        });
+      }
     }
   }
 }
